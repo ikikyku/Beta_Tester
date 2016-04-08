44464d10c3203f4be0aadbe7f393bee8a0fcbbad
ikikyku <venlawliet@gmail.com>
2016-04-08 13:12:20 +0700

    pemasukkan index.php

---
+++ index.php
@@ -0,0 +1,443 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" href="style/bootstrap.min.css">
+	<link rel="stylesheet" href="style/bootstrap-theme.min.css">
+</head>
+<body data-spy="scroll" data-target="#mynavbar1">
+
+<!--Navbar-->
+	<nav class="navbar navbar-inverse navbar-fixed-top" id="mynavbar1">
+		<div class="container">
+			<div class="navbar-header"><!--Navbar Brand and Burger Responsive Button-->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+
+				<a href="" class="navbar-brand">NdasGede</a>
+			</div><!--Navbar Brand and Burger Responsive Button-->
+
+			<div class="collapse navbar-collapse" id="navbar-collapse"><!--Navbar Menu NO BUTTON-->
+				<!--Navbar Menu BUTTON-->
+				
+				<!--Navbar Menu BUTTON-->
+
+				<ul class="nav navbar-nav">
+					<li><a href="#feedback">Feedback</a></li>
+					<li><a href="#gallery">Gallery</a></li>
+					<li><a href="#feature">Feature</a></li>
+					<li><a href="#faq">FAQ</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div><!--Navbar Menu-->
+
+		</div><!--Container End-->
+	</nav>
+<!--Navbar End-->
+
+<!--Jumbotron-->
+	<div class="jumbotron">
+		<div class="container text-center">
+			<h1>Ini Website Percobaan</h1>
+			<p>Ndase Guide Ndase Guide Ndase Guide Ndase Guide Ndase Guide Ndase Guide Ndase Guide </p>
+
+			<!--Button -->
+			<div class="btn-group">
+				<a class="btn btn-lg btn-warning">See All you wont</a>
+				<a class="btn btn-lg btn-info">Need Info</a>
+				<a class="btn btn-lg btn-default">Back to you</a>
+			</div><!--Button END-->
+
+		</div><!--Container END-->
+	</div>
+<!--Jumbotron END-->
+
+<!--Container Portofolio-->	
+	<div class="container">
+		<section>
+			<div class="page-header" id="feedback">
+				<h2>This is page for Feedback, Try it, <small>Make the first your tag Page Website</small></h2>
+			</div>
+
+			<!--ROW-->
+			<div class="row">
+				<div class="col-lg-4"><!--ROW NUMBER 1-->
+					<blockquote>
+						<p>
+							Give Attention to this Sentence and Learn It `Till you really really understand how it work
+							Dont be Surrender for Learn Something
+						</p>
+						<footer>
+							Me As Writer
+						</footer>
+					</blockquote>
+				</div><!--ROW NUMBER 1 END-->
+
+				<div class="col-lg-4"><!--ROW NUMBER 2-->
+					<blockquote>
+						<p>
+							Give Attention to this Sentence and Learn It `Till you really really understand how it work
+							Dont be Surrender for Learn Something
+						</p>
+						<footer>
+							Me As Writer
+						</footer>
+					</blockquote>
+				</div><!--ROW NUMBER 2 END-->
+
+				<div class="col-lg-4"><!--ROW NUMBER 3-->
+					<blockquote>
+						<p>
+							Give Attention to this Sentence and Learn It `Till you really really understand how it work
+							Dont be Surrender for Learn Something
+						</p>
+						<footer>
+							Me As Writer
+						</footer>
+					</blockquote>
+				</div><!--ROW NUMBER 3 END-->
+			</div><!--ROW END-->
+		</section>
+	</div>
+<!--Container Portofolio-->
+
+<!--Call To Action-->
+	<section>
+		<div class="well"><!--WELL-->
+			<div class="continer text-center">
+				<h3>Free My Portofolio To You, Let Introduce yourself First</h3>
+				<p>Enter your name and email</p>
+
+				<!--FORM-->
+				<form action="" class="form-inline">
+					<div class="form-group">
+						<label for="subscription">subscribe</label>
+						<input type="text" class="form-control" id="subscription" placeholder="Enter Your Name">
+					</div>
+
+					<div class="form-group">
+						<label for="Email">Email</label>
+						<input type="text" class="form-control" id="email" placeholder="Enter Your Email">
+					</div>
+					<button type="submit" class="btn btn-warning">Get In Touch</button>					
+				</form><!--FORM END-->
+
+			</div>
+		</div><!--WELL END-->
+	</section>
+<!--Call To Action END-->
+
+<!-- Gallery -->
+	<div class="container">
+		<section>
+			<div class="page-header" id="gallery">
+				<h2>Gallery <small>Check Awasome Gallery</small></h2>
+			</div>
+
+			<!-- Carousel PESAN TEMPAT-->
+			<div class="carousel slide" id="screenshot-carousel" data-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-target="#screenshot-carousel" data-slide-to="0" class="active"></li>
+					<li data-target="#screenshot-carousel" data-slide-to="1" ></li>
+					<li data-target="#screenshot-carousel" data-slide-to="2" ></li>
+				</ol>
+
+				<div class="carousel-inner"><!-- Carousel Inner ISI TEMPAT YANG SUDAH DI PESAN-->
+
+					<div class="item active">
+						<img src="img/1529306.jpg" alt="">
+						<div class="carousel-caption">
+							<h3> Image 1	</h3>
+							<p>	Look to Beautifull image this</p>
+						</div>
+					</div>
+
+					<div class="item">
+						<img src="img/Landscape-Wallpapers-2015.png" alt="">
+						<div class="carousel-caption">
+							<h3> Image 2	</h3>
+							<p>	Look to Beautifull image this</p>
+						</div>
+					</div>
+					
+					<div class="item">
+						<img src="img/mountain-landscape-wallpaper.jpg" alt="">
+						<div class="carousel-caption">
+							<h3> Image 3	</h3>
+							<p>	Look to Beautifull image this</p>
+						</div>
+					</div>
+
+				</div><!-- Carousel Inner END-->	
+				
+				<a href="#screenshot-carousel" class="left carousel-control" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+				</a>
+
+				<a href="#screenshot-carousel" class="right carousel-control" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+				</a>
+			</div><!-- Carousel END-->
+		</section>
+	</div>
+<!-- Gallery END-->
+
+<!-- Gallery END-->
+	<div class="container">
+		<section>
+			<div class="page-header" id="feature">
+				<h2>Gallery<small>See my creativity in here and found some idea from my arts</small></h2>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-8">
+					<h3>PRODUCT 1</h3>
+					<p>This is my Comunnity Will be Created Something cool and professionall</p>
+				</div>
+			
+				<div class="col-lg-4">
+					<img src="img/Website-Responsive-PNG-02986-540x302.png" class="img-responsive">
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-3"></div>
+
+				<div class="col-lg-4">
+					<img src="img/Responsive-Screen-Mockup-Pack.png" class="img-responsive">
+				</div>
+
+				<div class="col-lg-5">
+					<h3>PRODUCT 2</h3>
+					<p>This is my Comunnity Will be Created Something cool and professionall</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-5">
+					<h3>PRODUCT 3</h3>
+					<p>This is my Comunnity Will be Created Something cool and professionall</p>
+				</div>
+			
+				<div class="col-lg-4">
+					<img src="img/web-design.png" class="img-responsive">
+				</div>
+
+				<hr>
+				<div class="row">
+
+					<div class="col-lg-4">
+						<div class="panel panel-default text-center">
+							<div class="panel-body">
+								<span class="glyphicon glyphicon-search"></span>
+								<h3>Mungkin ini catatan kakinya hehehe</h3>
+								<p>Entahlah ini tentang apa yang penting terisi tulisan yang baik dan benar bisa dipahami
+									isi syntax maupun code tag HTMLnya :D </p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-lg-4">
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<span class="glyphicon glyphicon-cloud"></span>
+								<h3>Mungkin ini catatan kakinya hehehe</h3>
+								<p>Entahlah ini tentang apa yang penting terisi tulisan yang baik dan benar bisa dipahami
+									isi syntax maupun code tag HTMLnya :D </p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-lg-4">
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<span class="glyphicon glyphicon-heart"></span>
+								<h3>Mungkin ini catatan kakinya hehehe</h3>
+								<p>Entahlah ini tentang apa yang penting terisi tulisan yang baik dan benar bisa dipahami
+									isi syntax maupun code tag HTMLnya :D </p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				
+			</div>
+		</section>
+	</div>
+<!--CONTAINER END-->
+
+<!--MAKE FAQ-->
+	<div class="container">
+		<section>
+			
+			<div class="page-header" id="faq">
+				<h2>FAQ <small>Engaging The Consumers</small></h2>
+			</div>
+
+			<div class="panel-group" id="accordion">
+				
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<div class="panel-title">
+							<a href="#collapse-1" data-toggle="collapse" data-parent="#accordion">
+								Question One 1 ?
+							</a>
+						</div>
+						<div id="collapse-1" class="panel-collapse collapse in">
+							<div class="panel-body">
+								Lorem ndas gede mboh tulisan GJ campuran inggris indo jowo ndas sing gede gede
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<div class="panel-title">
+							<a href="#collapse-2" data-toggle="collapse" data-parent="#accordion">
+								Question Two 2 ?
+							</a>
+						</div>
+						<div id="collapse-2" class="panel-collapse collapse">
+							<div class="panel-body">
+								Lorem ndas gede mboh tulisan GJ campuran inggris indo jowo ndas sing gede gede
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<div class="panel-title">
+							<a href="#collapse-3" data-toggle="collapse" data-parent="#accordion">
+								Question Three 3 ?
+							</a>
+						</div>
+						<div id="collapse-3" class="panel-collapse collapse">
+							<div class="panel-body">
+								Lorem ndas gede mboh tulisan GJ campuran inggris indo jowo ndas sing gede gede
+							</div>
+						</div>
+					</div>
+				</div>
+			
+			</div>
+		</section>
+	</div>
+<!--MAKE FAQ END-->
+
+<!--CONTACT-->
+	<div class="container">
+		<section>
+			<div class="page-header" id="contact">
+				<h2>Contact Us <small>Not Enough Information, Contact Us</small></h2>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-4">
+					<p>Send us message or contact us from address below</p>
+					<address>
+						<strong>mysevendays Company Profile</strong></br>
+						Alamatmu lah</br>
+						mboh iki opo</br>
+						pokoe alamat sing bener</br>
+						ojo ngapusi pokkoe wes</br>
+					</address>
+				</div>
+
+				<div class="col-lg-8">
+					<form action="" class="form-horizontal">
+						<div class="form-group">
+							<label for="user-name" class="col-lg-2 control-label">
+								Name
+							</label>
+							<div class="col-lg-10">
+								<input type="text" class="form-control" id="user-name" placeholder="Enter Your name">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="user-email" class="col-lg-2 control-label">
+								Email
+							</label>
+							<div class="col-lg-10">
+								<input type="text" class="form-control" id="user-email" placeholder="Enter Your E-mail">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="user-url" class="col-lg-2 control-label">
+								Your Website
+							</label>
+							<div class="col-lg-10">
+								<input type="text" class="form-control" id="user-url" placeholder="Enter Your Website">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="user-url" class="col-lg-2 control-label">
+								Explanation
+							</label>
+							<div class="col-lg-10">
+								<textarea name="user-message" id="user-message" class="form-control" cols="20" rows="10"
+								placeholder="Your Message">	
+								</textarea>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-lg-10 col-lg-offset-2">
+								<button type="submit" class="btn btn-primary">Submit</button>
+							</div>
+						</div>
+
+					</form>					
+				</div>
+			</div>
+
+		</section>
+	</div>
+
+<!--CONTACT END-->
+<!--footer-->
+	<footer>
+		<hr>
+		<div class="continer text-center">
+			<h3>Free My Portofolio To You, Let Introduce yourself First</h3>
+			<p>Enter your name and email</p>
+
+			<!--FORM-->
+			<form action="" class="form-inline">
+				<div class="form-group">
+					<label for="subscription">subscribe</label>
+					<input type="text" class="form-control" id="subscription" placeholder="Enter Your Name">
+				</div>
+
+				<div class="form-group">
+					<label for="Email">Email</label>
+					<input type="text" class="form-control" id="email" placeholder="Enter Your Email">
+				</div>
+				<button type="submit" class="btn btn-warning">Get In Touch</button>	
+				<hr>				
+			</form><!--FORM END-->
+			<hr>
+			<ul class="list-inline">
+				<li><a href="twitter"></a></li>
+				<li><a href="facebook"></a></li>
+				<li><a href="youtube"></a></li>
+			</ul>
+			<p>&copy; ikikyku@2015</p>
+			</div>
+	</footer>
+
+<!--footer END-->
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
